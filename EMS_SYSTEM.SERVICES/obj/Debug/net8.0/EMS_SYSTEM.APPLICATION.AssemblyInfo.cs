a11a2ea159989e9b7b8f443c8a0616e8c4933d32
Mohamed Fadel <fadelmohamed445@gmail.com>
2024-04-19 02:49:00 +0200

    Update Layers Names

--- EMS_SYSTEM.SERVICES/obj/Debug/net8.0/EMS_SYSTEM.APPLICATION.AssemblyInfo.cs
+++ EMS_SYSTEM.SERVICES/obj/Debug/net8.0/EMS_SYSTEM.APPLICATION.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS_SYSTEM.APPLICATION")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c8f3861c3314d527d15afceded732959c91834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2019488cee17b4d313812a0746140b588831ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS_SYSTEM.APPLICATION")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS_SYSTEM.APPLICATION")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
