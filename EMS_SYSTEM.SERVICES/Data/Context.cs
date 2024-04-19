ac2019488cee17b4d313812a0746140b588831ac
Mohamed Fadel <fadelmohamed445@gmail.com>
2024-04-19 02:29:16 +0200

    Add Project Architecture (UI Layer ,Domain Layer , Application Layer)

---
+++ EMS_SYSTEM.SERVICES/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_SYSTEM.SERVICES.Data
+{
+    public class Context
+    {
+        public Context()
+        {
+            
+        }
+
+    }
+}
