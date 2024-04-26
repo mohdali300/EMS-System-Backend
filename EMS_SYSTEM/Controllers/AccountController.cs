ca10e1ead9a0d1b585a1445af24635c11231edbd
Menna Mohamed <mennamohammed178@gmail.com>
2024-04-26 22:38:22 +0300

    Change Password Endpoint

--- EMS_SYSTEM/Controllers/AccountController.cs
+++ EMS_SYSTEM/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
-﻿using EMS_SYSTEM.APPLICATION.Repositories.Interfaces;
+﻿using System.Security.Claims;
+using EMS_SYSTEM.APPLICATION.Repositories.Interfaces;
+using EMS_SYSTEM.DOMAIN.DTO;
 using EMS_SYSTEM.DOMAIN.DTO.LogIn;
+using EMS_SYSTEM.DOMAIN.DTO.PasswordSettings;
+using EMS_SYSTEM.DOMAIN.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EMS_SYSTEM.Controllers
@@ -10,9 +15,14 @@ namespace EMS_SYSTEM.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
-        public AccountController(IAccountService _accountService)
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AccountController(IAccountService _accountService , IHttpContextAccessor httpContextAccessor ,UserManager<ApplicationUser> userManager)
         {
             this._accountService=_accountService;
+            this.httpContextAccessor = httpContextAccessor;
+            this.userManager = userManager;
         }
         [HttpPost("LogIn")]
         public async Task<IActionResult> LogIn([FromBody]LogInDTO model)
@@ -27,6 +37,35 @@ namespace EMS_SYSTEM.Controllers
                 return BadRequest(new {Response.Message});
             }
             return BadRequest(ModelState);
+        }
+        [HttpPost("ChangePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDTO ChangePassword)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+            // Get Current User
+            ClaimsPrincipal userIdClaim = httpContextAccessor.HttpContext.User;
+            var user = await userManager.GetUserAsync(userIdClaim);
+            //User Not Found
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseDTO { Status = "Error", Message = "User doesn't Exists" });
+            }
+            var result = await userManager.ChangePasswordAsync(user, ChangePassword.CurrentPassword!, ChangePassword.NewPassword!);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return StatusCode(StatusCodes.Status200OK, new ResponseDTO { Status = "Success", Message = "Password Changed Successfully" });
+
         }
     }
 }
