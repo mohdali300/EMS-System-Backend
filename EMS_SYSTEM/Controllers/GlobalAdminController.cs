d50b15e011bd8aacd444fb631a78d6dd58eec6d7
mohdali300 <mohdali30060@gmail.com>
2024-06-19 01:00:40 +0300

    Add GlobalAdmin Controller (المرة دي انا اللي نسيت اعمله معاهم)

---
+++ EMS_SYSTEM/Controllers/GlobalAdminController.cs
@@ -0,0 +1,18 @@
+﻿using EMS_SYSTEM.APPLICATION.Repositories.Interfaces.IUnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS_SYSTEM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GlobalAdminController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GlobalAdminController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
