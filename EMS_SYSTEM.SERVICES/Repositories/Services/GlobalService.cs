be1468dedeea3e20e36d85400f15ab78ebcc4b96
mohdali300 <mohdali30060@gmail.com>
2024-06-19 00:47:22 +0300

    Add GlobalAdmin files

---
+++ EMS_SYSTEM.SERVICES/Repositories/Services/GlobalService.cs
@@ -0,0 +1,21 @@
+﻿using EMS_SYSTEM.APPLICATION.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_SYSTEM.APPLICATION.Repositories.Services
+{
+    public class GlobalService:IGlobalService
+    {
+        private readonly UnvcenteralDataBaseContext _context;
+
+        public GlobalService(UnvcenteralDataBaseContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
